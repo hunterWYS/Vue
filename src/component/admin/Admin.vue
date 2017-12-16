95e732b5e614156623085fee4ed7122db429bf9a
wys <wys21502032@qq.com>
2017-12-16 21:43:34 +0800

    修改

---
+++ src/component/admin/Admin.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <!-- 左侧公共的导航栏 -->
+      <section>
+          <aside></aside>
+      </section>
+
+      <!-- 右侧的公共栏 -->
+      <section>
+          <!-- 公共的头部 -->
+          <header></header>
+          <!-- 动态局域，通过自路由来控制 -->
+         <article></article>
+      </section>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 //导入被路由的组件
 import HomeComponent from '../component/home/Home.vue';
 import LoginComponent from '../component/login/Login.vue';
+import AdminComponent from '../component/admin/Admin.vue';
 
 //对外导出路由配置对象
 export default {
     routes:[
         //首页
-        {name:'h',path:'/home',component: HomeComponent},
+        {name:'h',path:'/',component: AdminComponent,children:[
+
+        ]},
         //登录
         {name:'l',path:'/login',component: LoginComponent},
     ]
